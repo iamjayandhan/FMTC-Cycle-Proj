3b93ec306a72122dbbb6a3b14718051a28463459
guruprasath-v <guruprasathv.dev@gmail.com>
2024-11-08 23:03:52 +0530

    feat: Login Logics have been done in backend.

--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const userControllers = require('../controllers/userController');
 
 router.post('/', userControllers.userRegisterController);
+router.post('/login', userControllers.userLogin);
 
 module.exports = router;
