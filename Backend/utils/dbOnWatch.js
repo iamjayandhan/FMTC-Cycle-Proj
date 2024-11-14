887decf68a92dd717bb2face80b970f376b8964f
guruprasath-v <guruprasathv.dev@gmail.com>
2024-11-14 12:50:06 +0530

    v1 completed

--- Backend/utils/dbOnWatch.js
+++ Backend/utils/dbOnWatch.js
@@ -2,14 +2,14 @@ const { realTimeDb } = require('../confs/dbConfig');
 const cycleController = require('../controllers/cycleController');
 
 function dbOnWatch() {
-    const cyclesRef = realTimeDb.ref('CYCLE_LOCKED');
+    const cyclesRef = realTimeDb.ref('CYCLE_UNLOCK');
 
-    cyclesRef.on('child_changed', (snapshot) => {
+    cyclesRef.on('value', (snapshot) => {
         setTimeout(async () => { // Use setTimeout inside the callback
             console.log(`After 5 seconds...`);
             const data = snapshot.val();
             console.log(`data_changed: ${JSON.stringify(data, null, 2)}`); // Pretty-print JSON
-            if (data.parent.STATUS === "LOCKED") {
+            if (data.STATUS === "LOCKED") {
                 await cycleController.lockCycle(data.CYCLE, data.STAND_ID, data.SLOT_ID);
             }
         }, 5000);
